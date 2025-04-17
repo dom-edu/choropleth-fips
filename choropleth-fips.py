d2607ce94e0d6632763ce2733f384663ce4d7084
Brian McClain <162603666+Militant1@users.noreply.github.com>
2025-04-16 20:49:24 -0400

    added range slider, added callback

--- choropleth-fips.py
+++ choropleth-fips.py
@@ -2,7 +2,7 @@ from urllib.request import urlopen
 import json
 import pandas as pd
 import plotly.express as px
-from dash import Dash, html , dcc
+from dash import Dash, html , dcc, callback, Input, Output
 
 # specify data sources 
 COUNTIES_FIPS_URL = 'https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json'
@@ -26,7 +26,14 @@ choro = px.choropleth(fips_df, geojson=counties, locations='fips', color='unemp'
 choro.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
 # add range slider 
-rs1 = dcc.RangeSlider(0, 12, value=[2, 4], id="range-slider-1", included=True)
+rs1 = dcc.RangeSlider(
+    0, 
+    12, 
+    value=[2, 4], 
+    id="range-slider-1", 
+    included=True,
+    tooltip={'always_visible':True}
+    )
 
 
 # instantiate app 
@@ -34,9 +41,40 @@ app = Dash()
 
 app.layout = [
     html.H1("Unemployment Rate By FIPS County ", style={'textAlign':'center'}),
-    dcc.Graph(figure=choro),
+    dcc.Graph(figure=choro, id="fips-choropleth"),
     rs1 
 ]
+# setup callback
+@callback(
+    Output("fips-choropleth","figure"),
+    Input("range-slider-1","value")
+)
+def update_graph(value):
+    print(value)
+
+
+    # filter counties by range values 
+    # is the counties value between the start and the end?
+
+    start = value[0]
+    end = value[1]
+
+    # greater than the start of slider point
+    filter_ = fips_df['unemp'] >= start
+    # less than the end slider point 
+    filter_2 = (fips_df['unemp'] <= end)
+    choro_filtered_df = fips_df[filter_ & filter_2]
+
+
+
+    choro = px.choropleth(choro_filtered_df, geojson=counties, locations='fips', color='unemp',
+                            color_continuous_scale="Viridis",
+                            range_color=(0, 12),
+                            scope="usa",
+                            labels={'unemp':'unemployment rate'}
+                          )
+
+    return choro 
 
 if __name__ == '__main__':
     app.run(port=5008, debug=True)
