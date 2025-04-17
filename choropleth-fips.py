0d8840fbdf221f21e7e551f50828c18826ed9972
Brian McClain <162603666+Militant1@users.noreply.github.com>
2025-04-16 20:08:35 -0400

    added Choropleth visual to dash app

--- choropleth-fips.py
+++ choropleth-fips.py
@@ -2,7 +2,7 @@ from urllib.request import urlopen
 import json
 import pandas as pd
 import plotly.express as px
-from dash import Dash, html 
+from dash import Dash, html , dcc
 
 # specify data sources 
 COUNTIES_FIPS_URL = 'https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json'
@@ -17,20 +17,20 @@ fips_df = pd.read_csv(FIPS_UNEMP_RATES, dtype={"fips": str})
 
 
 # create figure 
-fig = px.choropleth(fips_df, geojson=counties, locations='fips', color='unemp',
+choro = px.choropleth(fips_df, geojson=counties, locations='fips', color='unemp',
                            color_continuous_scale="Viridis",
                            range_color=(0, 12),
                            scope="usa",
                            labels={'unemp':'unemployment rate'}
                           )
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+choro.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
 # instantiate app 
 app = Dash() 
 
 app.layout = [
     html.H1("Unemployment Rate By FIPS County ", style={'textAlign':'center'}),
-    
+    dcc.Graph(figure=choro)
 ]
 
 if __name__ == '__main__':
